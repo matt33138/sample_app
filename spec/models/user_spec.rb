a20df31a3bac15006fd9d99c366aee88ee7a4019
Matt Coffman <mattcoff@gmail.com>
2013-06-22 13:48:04 -0500

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,7 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com", 
+    @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
 
@@ -28,7 +28,7 @@ describe User do
   it { should be_valid }
   it { should respond_to(:authenticate) }
 
-  describe "when name is not present" do
+ describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
   end
@@ -45,9 +45,8 @@ describe User do
 
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                    foo@bar_vaz.com foo@bar+baz.com]
-      addresses.each do | invalid_address |
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+      addresses.each do |invalid_address|
         @user.email = invalid_address
         @user.should_not be_valid
       end
@@ -56,8 +55,8 @@ describe User do
 
   describe "when email format is valid" do
     it "should be valid" do
-      addresses = %w[user@foo.COM a_us-er@f.b.org frst.lst@foo.jp a+b@baz.cn]
-      addresses.each do | valid_address |
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
         @user.email = valid_address
         @user.should be_valid
       end
@@ -71,6 +70,19 @@ describe User do
       user_with_same_email.save
     end
 
+    it { should_not be_valid }
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
@@ -79,13 +91,13 @@ describe User do
   describe "when password doesn't match confirmation" do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
-  end  
+  end
 
   describe "when password confirmation is nil" do
     before { @user.password_confirmation = nil }
     it { should_not be_valid }
   end
- 
+
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
@@ -107,4 +119,3 @@ describe User do
     end
   end
 end
-end 
